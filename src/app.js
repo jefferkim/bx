3a9ace664215383e0e4c9196dd06f97361134747
tancy <ronntx@gmail.com>
2013-01-21 13:53:28 +0800

    project skeleton

---
+++ src/app.js
@@ -0,0 +1,44 @@
+seajs.config({
+    "alias":{
+    "backbone": "backbone/0.9.2/backbone",
+    "underscore": "underscore/1.2.2/underscore",
+    "mu": "mustache/0.4.0/mu",
+    "zepto": "zepto/1.0.0/zepto"
+    }
+});
+
+
+define(function(require, exports){
+    var _ = require('underscore'),
+        $ = require('zepto'),
+        Backbone = require('backbone');
+
+    var Router = Backbone.Router.extend({
+        routes:{
+            "":"home",
+            "home":"home",
+            "list":"list"
+            
+        },
+        home:function(){
+            console.log('home');
+        },
+        list:function(){
+            console.log('list');
+        }
+
+    });
+    // 初始化Router
+    // var Start = function(){
+    //     var app_router = new Router;
+    //     // 开启Backbone的历史功能
+    //     Backbone.history.start();
+    // };
+   
+    return {init:function(){
+        console.log('init');
+        var app_router = new Router;
+
+        Backbone.history.start();
+    }};
+});
---
+++ src/build-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "loader_config": "./app.js",
+    "base_path": "../../../base/libs",
+    "app_url" : "/mw/app/transformer/"   
+}
