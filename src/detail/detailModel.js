1bfd59749f28543054e166b81e04e97ec18e7919
unknown <qianju@t-qianju.taobao.ali.com>
2013-02-22 10:44:19 +0800

    add detail model

---
+++ src/detail/detailModel.js
@@ -0,0 +1,59 @@
+define(function (require, exports, module) {
+        var Backbone = require('backbone'),
+        _ = require('underscore'),
+        mtop = require('../common/mtopForAllspark.js');
+        cache = require('../common/cache.js')
+
+    /**
+     * 详情页面
+     */
+    return Backbone.Model.extend({
+        /**
+         * mtop.sns.feed.detail Feed详情
+         用户登录可选
+         入参：
+         snsId: string
+         feedId:string
+         出参：
+         BatOperationResult feed详情
+         业务异常：sid不存在对应的snsAccount，feed不存在，sid没有权限，
+         返回特定错误码(FAIL_DOWNGRADED)表示被降级
+           */
+        getPageData:function (param) {
+
+            function getPrices(result) {
+                  //获取价格参数
+                  var ids = [];
+                  result.tiles.forEach(function(tile){tile.items   && tile.items.length &&
+                        tile.items.forEach(function(item){
+                              ids.push(item.id); }
+                        )
+                  });
+                  mtop.getPrices(_.uniq(ids),function(prices){
+                      prices.length && self.set("prices",prices);
+                  })
+             }
+
+            var self = this;
+            var cacheKey = param.snsId+"_" +param.feedId
+            var cacheFeed = cache.getItemById(cacheKey);
+            if (cacheFeed) {
+                self.set({
+                        "feed": cacheFeed
+                    });
+                    getPrices(cacheFeed);
+                    return;
+            }
+            else {
+                mtop.getData("mtop.sns.feed.detail", param || {}, function (result) {
+                    self.set("feed",result.data);
+                    cache.saveItem(cacheKey,result.data);
+                    getPrices(result.data);
+                }, function (result) {
+                    self.set("feed","fail");
+                  }
+                );
+            }
+    }
+});
+});
