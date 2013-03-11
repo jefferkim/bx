8e3132cebff1a0ac8bdbcab791aefde77a8c25c1
jiawulu <jiawu.lu@gmail.com>
2013-03-11 14:11:37 +0800

    total count

--- src/dynIndex/dynIndexModel.js
+++ src/dynIndex/dynIndexModel.js
@@ -133,7 +133,7 @@ define(function (require, exports, module) {
                 if (result.succ && 1 == type) {
 
                     getPubAccounts(pageParam, pageParam.isIndex() ? function (accResult) {
-                        if (accResult.totalCount && parseInt(accResult.totalCount) <= 1) {
+                        if ( accResult.list.length <= 1) {
                             getrecommends(pageParam);
                         }
                     } : null);
