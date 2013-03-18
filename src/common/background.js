135c3767c7d4825cd8cada93f89954c04ce4f19c
jiawulu <jiawu.lu@gmail.com>
2013-03-18 15:42:36 +0800

    增加埋点及图片高度问题

---
+++ src/common/background.js
@@ -0,0 +1,86 @@
+define(function (require, exports, module) {
+
+    var log = require("./log.js"),
+        Backbone = require('backbone'),
+        hashStack = [],
+        h5_comm = require('h5_comm'),
+        times = -1;
+
+    function addHash(hash,origin){
+        hashStack.push({hash: hash, hisLen: ++times, orignHash: origin ? origin : location.hash, isFromBack: function () {
+            return  times - this.hisLen != 1;
+        }});
+    }
+    addHash("index","#index");
+
+    /**
+     * 记录用户进入该页面,同时可以触发相关的log
+     * @param hash
+     */
+    exports.enter = function (hash) {
+        var tmpStack = [];
+        var i = 0;
+        while (i < hashStack.length) {
+            if (hashStack[i].hash == hash) {
+                break;
+            } else {
+                tmpStack.push(hashStack[i])
+            }
+            i++;
+        }
+        hashStack = tmpStack;
+        addHash(hash);
+
+        //记录进入日志
+        logEnter && logEnter(hash);
+    }
+
+    function logEnter(hash){
+        if(hashStack.length > 1){
+            var lastHash = hashStack[hashStack.length - 2];
+            lastHash.isFromBack() || log.logEnter(hash, isFromLogin() ? "login":"");
+        }else{
+            log.logEnter(hash, isFromLogin() ? "login":"");
+        }
+    }
+
+    function isFromLogin(){
+        return h5_comm.isLogin() && 1 == times
+            && document.referrer && 0 == document.referrer.indexOf("http://login");
+    }
+
+    var BackgroundView = Backbone.View.extend({
+        el: '#content',
+        events: {
+            'click header .back': function (e) {
+                var prevHash = null;
+                if (hashStack.length > 1) {
+                    prevHash = hashStack[hashStack.length - 2];
+                }
+
+                window.location.href = ((prevHash && prevHash.orignHash) ? prevHash.orignHash : "#index");
+                return false;
+            },
+            'click .log[data-log]': function (e, r) {
+                var currentUri = hashStack.length ?  hashStack[hashStack.length - 1].hash : "";
+                var z = $(e.currentTarget);
+                var data = z.data("log")
+                if(data){
+                    if( !h5_comm.isLogin() && (data == "addaccount" || data == "attention" || data == "addcomment")){
+                        //log to login
+                        log.logToLogin(data,currentUri);
+                    }
+                    if("attention" == data && "account" == currentUri && z.hasClass("followed")){
+                        data = "cancelattention";
+                    }
+                    log.logClick(data,currentUri);
+                    console.log(data);
+                }
+
+            }
+        }
+    });
+
+    exports.backgroundView = new BackgroundView;
+
+});
--- test/cdn/test.js
+++ test/cdn/test.js
@@ -1,12 +1,12 @@
 seajs.config({
-    alias:{
-        'backbone':'http://a.tbcdn.cn/mw/base/libs/backbone/0.9.2/backbone',
-        'underscore':'http://a.tbcdn.cn/mw/base/libs/underscore/1.3.3/underscore',
-        'zepto':'http://a.tbcdn.cn/mw/base/libs/zepto/1.0.0/zepto',
-        'mustache':'http://a.tbcdn.cn/mw/base/libs/mustache/0.5.0/mustache',
-        'cdn':'../../../../base/utils/server/cdn'
+    alias: {
+        'backbone': 'http://a.tbcdn.cn/mw/base/libs/backbone/0.9.2/backbone',
+        'underscore': 'http://a.tbcdn.cn/mw/base/libs/underscore/1.3.3/underscore',
+        'zepto': 'http://a.tbcdn.cn/mw/base/libs/zepto/1.0.0/zepto',
+        'mustache': 'http://a.tbcdn.cn/mw/base/libs/mustache/0.5.0/mustache',
+        'cdn': '../../../../base/utils/server/cdn'
     },
-    debug:1
+    debug: 1
 });
 
 
@@ -31,6 +31,66 @@ define(function (require, exports) {
 
     console.log(cdn.getBetterImg("http://img02.taobaocdn.com/tps/i2/T1b9JqXshaXXcebizh-990-200.jpg", 100, 140));
 
+
+
+    cdn.setDefaultDpi(2);
+//    console.log(cdn.resizeImg({
+//        url: "http://img02.taobaocdn.com/tps/i2/T1b9JqXshaXXcebizh-990-200.jpg",
+//        rWidth: 480,
+//        rHeight: 990,
+//        expWidth: 320 }));
+//    cdn.setDefaultDpi(2);
+//    console.log(cdn.resizeImg({
+//        url: "http://img02.taobaocdn.com/tps/i2/T1b9JqXshaXXcebizh-990-200.jpg",
+//        rWidth: 480,
+//        rHeight: 990,
+//        expWidth: 320 }));
+//    cdn.setDefaultDpi(2);
+    console.log(cdn.resizeImg({
+        url: "http://img02.taobaocdn.com/tps/i2/T1b9JqXshaXXcebizh-990-200.jpg",
+        rWidth: 990,
+        rHeight: 990,
+        expWidth: 320 }));
+
+    console.log(cdn.resizeImg({
+        url: "http://img02.taobaocdn.com/tps/i2/T1b9JqXshaXXcebizh-990-200.jpg",
+        rWidth: 990,
+        rHeight: 990,
+        expWidth: 320,
+        isXz: true}));
+    console.log(cdn.resizeImg({
+        url: "http://img02.taobaocdn.com/tps/i2/T1b9JqXshaXXcebizh-990-200.jpg",
+        rWidth: 990,
+        rHeight: 1500,
+        expWidth: 320,
+        isXz: true}));
+    console.log(cdn.resizeImg({
+        url: "http://img02.taobaocdn.com/tps/i2/T1b9JqXshaXXcebizh-990-200.jpg",
+        rWidth: 990,
+        rHeight: 1500,
+        expWidth: 320}));
+    console.log(cdn.resizeImg({
+        url: "http://img02.taobaocdn.com/tps/i2/T1b9JqXshaXXcebizh-990-200.jpg",
+        rWidth: 990,
+        rHeight: 400,
+        expWidth: 320,
+        isXz: true}));
+    console.log(cdn.resizeImg({
+        url: "http://img02.taobaocdn.com/tps/i2/T1b9JqXshaXXcebizh-990-200.jpg",
+        rWidth: 990,
+        rHeight: 400,
+        expWidth: 320}));
+    console.log(cdn.resizeImg({
+        url: "http://img02.taobaocdn.com/tps/i2/T1b9JqXshaXXcebizh-990-200.jpg",
+        rWidth: 480,
+        rHeight: 400,
+        expWidth: 320,
+        isXz: true}));
+    console.log(cdn.resizeImg({
+        url: "http://img02.taobaocdn.com/tps/i2/T1b9JqXshaXXcebizh-990-200.jpg",
+        rWidth: 480,
+        rHeight: 900,
+        expWidth: 320}));
 
 });
 
