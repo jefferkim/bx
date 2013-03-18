43c3234d96a1f2cd1f492331438782d662c0004a
qianju <qianju@taobao.com>
2013-03-18 16:32:50 +0800

    m

--- src/account/accountModel.js
+++ src/account/accountModel.js
@@ -76,6 +76,7 @@ define(function (require, exports, module) {
                 mtop.getPrices(_.uniq(ids),function(prices){
                     prices.length && self.set(prices,{'prices':prices,'t':new Date().getTime()});
                 })
+                self.set("loaded","1");
             })
         }
 
