761da88f719f78382a620d27d8c38337817cdfad
Tom Wong <zuoshi.why@gmail.com>
2013-04-09 14:35:36 +0800

    render index header

--- src/index/indexView.js
+++ src/index/indexView.js
@@ -4,45 +4,46 @@
  * Time: AM11:18
  */
 define(function (require, exports, module) {
-    var Backbone = require('backbone'),
-        $ = require('zepto'),
-        _ = require('underscore'),
-        _model=require('./indexModel'),
-        cdn = require('cdn'),
-        h5_base = require('h5_base'),
-        uriBroker = require('uriBroker'),
-        loading = require('../ui/loading');
-  
+  var Backbone = require('backbone'),
+      $ = require('zepto'),
+      _ = require('underscore'),
+      _model=require('./indexModel'),
+      cdn = require('cdn'),
+      h5_base = require('h5_base'),
+      uriBroker = require('uriBroker'),
+      loading = require('../ui/loading');
 
+    var header = $('#index_header_tpl').html()
 
+   return Backbone.View.extend({
 
-   var detailView = Backbone.View.extend({
+    el: '#content',
+    model : new _model(),
+    events:{
 
-        el: '#indexPage',
-        model : new _model(),
-        events:{
-          
-        },
-        initialize:function () {
-            this.model.on('change:params', this.queryFeeds, this);
-           
-        },
-		render:function(){
-			var params={
-				timestamp:0,//Date.now(),
-				curPage:1,
-				pageSize :20,
-				onlyYou:1,
-				direction:0
-			};
-			this.model.getPageData(params);
+    },
+    initialize:function () {
+      this.params = {
+          timestamp:0,//Date.now(),
+          curPage:1,
+          pageSize :20,
+          onlyYou:1,
+          direction:0
+      }
+      this.model.on('change:timeLine', this.renderFeeds, this);
+    },
+
+		render:function(page){
+      this.params.curPage = page
+			this.model.getPageData(this.params)
+
+      $('.navbar').html(header)
 		},
-       queryFeeds : function(par){
-	   	
-	   	 this.model.getPageData(par);
- 		}
 
-    });
-    return detailView;
-}
-);
+    renderFeeds: function() {
+
+    }
+
+
+  });
+});
