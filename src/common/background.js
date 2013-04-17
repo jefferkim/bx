1917f9a39a35269143a4b5e519e7c02677586046
jiawulu <jiawu.lu@gmail.com>
2013-04-17 16:23:19 +0800

    update back logic

--- src/common/background.js
+++ src/common/background.js
@@ -2,86 +2,50 @@ define(function (require, exports, module) {
 
     var log = require("./log.js"),
         Backbone = require('backbone'),
-        hashStack = [],
         h5_comm = require('h5_comm'),
-        times = -1;
+        tbh5 = require('h5_base'),
+        History = require('../../../../base/modules/history/history.js');
 
-    function addHash(hash,origin){
-        hashStack.push({hash: hash, hisLen: ++times, orignHash: origin ? origin : location.hash, isFromBack: function () {
-            return  times - this.hisLen != 1;
-        }});
-    }
-    addHash("index","#index");
-
-    /**
-     * 记录用户进入该页面,同时可以触发相关的log
-     * @param hash
-     */
-    exports.enter = function (hash) {
-        var tmpStack = [];
-        var i = 0;
-        while (i < hashStack.length) {
-            if (hashStack[i].hash == hash) {
-                break;
-            } else {
-                tmpStack.push(hashStack[i])
-            }
-            i++;
+    History.prototype.extend({
+        defaultPage: "http://m.taobao.com",
+        defaultHash:"#index",
+        defaultSeparator : '/',
+        fromBack: function(){
+            return h5_comm.isLogin() && document.referrer && 0 == document.referrer.indexOf("http://login");
+        },
+        setItem:function(key,value){
+            tbh5.set(key,value);
+        },
+        getItem:function(key){
+            return tbh5.get(key);
+        },
+        validate:function(){
+            return location.hash.indexOf("&") == -1;
         }
-        hashStack = tmpStack;
-        addHash(hash);
-
-        //记录进入日志
-        //logEnter && logEnter(hash);
-    }
-
-    function logEnter(hash){
-        if(hashStack.length > 1){
-            var lastHash = hashStack[hashStack.length - 2];
-            lastHash.isFromBack() || log.logEnter(hash, isFromLogin() ? "login":"");
-        }else{
-            log.logEnter(hash, isFromLogin() ? "login":"");
-        }
-    }
-
-    function isFromLogin(){
-        return h5_comm.isLogin() && 1 == times
-            && document.referrer && 0 == document.referrer.indexOf("http://login");
-    }
+    });
+    window.history = new History;
 
     var BackgroundView = Backbone.View.extend({
         el: '#content',
         events: {
-            'click header .back': function (e) {
-                if(hashStack.length > 0 && "index" == hashStack[hashStack.length - 1].hash ){
-                    return true;
-                }
-                var prevHash = null;
-                if (hashStack.length > 1) {
-                    prevHash = hashStack[hashStack.length - 2];
-                }
-                window.location.href = ((prevHash && prevHash.orignHash) ? prevHash.orignHash : "#index");
+            'click header .back': function () {
+                history.back();
                 return false;
             },
-            'click .log[data-log]': function (e, r) {
-                var currentUri = hashStack.length ?  hashStack[hashStack.length - 1].hash : "";
+            'click .log[data-log]': function (e) {
+                var currentUri = location.hash ? location.hash.split("/")[0].replace("#","") : "";
                 var z = $(e.currentTarget);
                 var data = z.data("log")
                 if(data){
-                    if( !h5_comm.isLogin() && (data == "addaccount" || data == "attention" || data == "addcomment")){
-                        //log to login
-                        log.logToLogin(data,currentUri);
-                    }
                     if("attention" == data && "account" == currentUri && z.hasClass("followed")){
                         data = "cancelattention";
                     }
                     log.logClick(data,currentUri);
                 }
-
             }
         }
     });
 
-    exports.backgroundView = new BackgroundView;
+    new BackgroundView;
 
 });
