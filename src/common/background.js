c83f85a4bc4344a4584691eca7e0bc155ad3894e
luu <jiawu.lu@gmail.com>
2013-04-22 22:24:34 +0800

    update it

--- src/common/background.js
+++ src/common/background.js
@@ -4,47 +4,35 @@ define(function (require, exports, module) {
         Backbone = require('backbone'),
         h5_comm = require('h5_comm'),
         tbh5 = require('h5_base'),
-        History = require('../../../../base/modules/history/history.js'),
-        login_refer_flag = localStorage ? localStorage.getItem("f_l") : false;
-
-    login_refer_flag && localStorage.removeItem("f_l");
-
+        History = require('../../../../base/modules/history/history.js');
 
     History.prototype.extend({
-        defaultPage: "http://m.taobao.com",
-        defaultHash: "#index",
-        defaultSeparator: '/',
-        fromBack: function () {
-            if (login_refer_flag) {
-                delete login_refer_flag;
-                return true;
-            } else {
-                return h5_comm.isLogin() && document.referrer && 0 == document.referrer.indexOf("http://login");
-            }
+        defaultPage:"http://m.taobao.com",
+        defaultHash:"#index",
+        defaultSeparator:'/',
+        fromBack:function () {
+            return h5_comm.isLogin() && 0 == document.getRefer().indexOf("http://login");
         },
-        setItem: function (key, value) {
+        setItem:function (key, value) {
             tbh5.set(key, value);
         },
-        getItem: function (key) {
+        getItem:function (key) {
             return tbh5.get(key);
         },
-        validate: function () {
+        validate:function () {
             return location.hash.indexOf("&") == -1;
-        },
-        filterEntrance: function(entrence){
-            return entrence ? ( entrence.indexOf(location.pathname) >= 0 ? "" : document.referrer) : entrence;
         }
     });
 
     var myHis = new History;
     var BackgroundView = Backbone.View.extend({
-        el: '#content',
-        events: {
-            'click header .back': function () {
+        el:'#content',
+        events:{
+            'click header .back':function () {
                 myHis.back();
                 return false;
             },
-            'click .log[data-log]': function (e) {
+            'click .log[data-log]':function (e) {
                 var currentUri = location.hash ? location.hash.split("/")[0].replace("#", "") : "";
                 var z = $(e.currentTarget);
                 var data = z.data("log")
