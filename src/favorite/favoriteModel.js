dcd48b0f6a60199ef30bee556f03b2518a2efd2d
unknown <lanyin.smz@ALI-39753M.taobao.ali.com>
2013-05-13 11:52:57 +0800

    update

--- src/favorite/favoriteModel.js
+++ src/favorite/favoriteModel.js
@@ -11,43 +11,44 @@ define(function (require, exports, module) {
 		  favoriteFeeds:function (param) {
 		  	var self=this;
              mtop.favoriteFeeds(param, function (recResult) {
-                  console.log(recResult);
+                  //console.log(recResult);				  
+					recResult.t=new Date().getTime();	
                     if(recResult.fail){                       
 						recResult.errMsg='服务器繁忙，请稍后再试！';
+						recResult.t=new Date().getTime();
 						 self.set("feedsList",recResult);
                         return;
                     }
-                    recResult.t=new Date().getTime();
                     self.set("feedsList", recResult);                   
                 })
             },
 			addFeed:function (param) {
 				var self=this;
-                mtop.favoriteAddFeed(param, function (recResult) { 
-                    if(recResult.fail){						
+                mtop.favoriteAddFeed(param, function (recResult) { 				
+					recResult.t=new Date().getTime();	
+                    if(recResult.fail){					
 						if(recResult.fail.indexOf('FAIL_BIZ_SNS_FAVORITE_ALREADY_FAVORITE')>-1)	{
-						recResult.errMsg="对不起，该广播您已经收藏过！";
-					}else if(recResult.fail.indexOf('FAIL_BIZ_SNS_FAVORITE_NOT_EXSIT')> -1){
-						recResult.errMsg="对不起，该广播不存在！";
-					}else{
-						recResult.errMsg='服务器繁忙，请稍后再试！';
-					}	
+							recResult.errMsg="对不起，该广播您已经收藏过！";
+						}else if(recResult.fail.indexOf('FAIL_BIZ_SNS_FAVORITE_NOT_EXSIT')> -1){
+							recResult.errMsg="对不起，该广播不存在！";
+						}else{
+							recResult.errMsg='服务器繁忙，请稍后再试！';
+						}	
                         self.set("favStatus",recResult);
                         return;
                     }
-                    recResult.t=new Date().getTime();
                     self.set("favStatus", recResult);                   
                 })
             },
 			removeFeed:function (param) {
 				var self=this;
                 mtop.favoriteRemoveFeed(param, function (recResult) { 
+					recResult.t=new Date().getTime();
                     if(recResult.fail){	
 						recResult.errMsg='服务器繁忙，请稍后再试！';
                         self.set("favStatus",recResult);
                         return;
                     }
-                    recResult.t=new Date().getTime();
                     self.set("favStatus", recResult);
                 })
             }
